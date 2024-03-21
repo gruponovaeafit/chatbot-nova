b4bdd08622b2ffb7d04468e2441aa21f7322aa12
Lina Ballesteros <lina.ballesteros13@gmail.com>
2024-03-20 19:49:37 -0500

    added new templates for loading pages, auto-scrolling, hack version, animations

--- code/backend/app.py
+++ code/backend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, jsonify
-
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+import time
 # import torch
 import NovaBot as nv
 
@@ -10,11 +10,25 @@ import NovaBot as nv
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route("/chat_hack")
+def chat_hack():
+    return render_template("chat_hack.html")
+
+@app.route("/nova_pacho")
+def nova_pacho():
+    return render_template("pacho_loading.html")
+
+@app.route("/chat")
+def nova_logo():
     return render_template("chat.html")
 
+@app.route("/") 
+def index():
+    return render_template("nova_logo.html")
+
 @app.route("/get", methods=["GET", "POST"])
+
+
 
 def chat():
     msg = request.form["msg"]
