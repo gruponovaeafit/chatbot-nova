f4b6dc0486265537b22c862aa330d0f6f2b93ecc
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-08-27 07:57:10 -0500

    Refactor routes and functions and implement routing via function name

--- app.py
+++ app.py
@@ -1,47 +1,51 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 import time
 # import torch
-import NovaBot as nv
+import model.nova_bot as nv
 
 app = Flask(__name__)
 
-@app.route("/chat_hack")
-def chat_hack():
-    return render_template("chat_hack.html")
+
+@app.route("/")
+def chat_nova():
+    template_data = {}
+    template_data["title"] = "NOVA ChatBot"
+    return render_template("chat_nova.html", template_data=template_data)
+
+
+@app.route("/omegahack")
+def chat_omegahack():
+    template_data = {}
+    template_data["title"] = "NOVA OmegaBot"
+    return render_template("chat_omegahack.html", template_data=template_data)
+
 
 @app.route("/nova_pacho")
 def nova_pacho():
     return render_template("pacho_loading.html")
 
-@app.route("/chat")
-def nova_logo():
-    return render_template("chat.html")
 
 @app.route("/nova_logo")
 def nova_logo_():
     return render_template("nova_logo.html")
 
+
 @app.route("/nova_logo2")
 def nova_logo_2():
     return render_template("nova_logo2.html")
 
-@app.route("/omega_logo")
+
+@app.route("/omega-logo")
 def omega_logo():
-    return render_template("omega_logo.html")
+    return render_template("omegahack/logo.html")
 
-
-@app.route("/") 
-def index():
-    return render_template("nova_logo.html")
 
 @app.route("/get", methods=["GET", "POST"])
-
-
-
 def chat():
     msg = request.form["msg"]
     input = msg
     return nv.generate_response(input)
+
 
 if __name__ == '__main__':
     app.run()
