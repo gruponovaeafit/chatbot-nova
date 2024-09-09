d9a732fa044adec2e74b1d583eb867309f2297ab
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-09 14:40:59 -0500

    Delete Chatbot route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Nova from "./views/Nova";
 import Next from "./views/Next";
 import Omegahack from "./views/Omegahack";
-import Chatbot from "./views/Chatbot";
 import "./App.css";
 
 export default function App() {
@@ -19,7 +18,6 @@ function AppContent() {
         <Route path="/" exact element={<Nova />} />
         <Route path="/next" element={<Next />} />
         <Route path="/omegahack" element={<Omegahack />} />
-        <Route path="/chatbot" element={<Chatbot />} />
       </Routes>
   );
 }
