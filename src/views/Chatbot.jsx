4ce4d3782dc4755267bced8cfa69670da75a94e1
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-09 10:12:24 -0500

    Add scrollable messages container

--- src/views/Chatbot.jsx
+++ src/views/Chatbot.jsx
@@ -1,58 +1,96 @@
-import React from "react";
 import "../css/Chatbot.css";
 import logoPacho from "../assets/pacho.svg";
 import userIcon from "../assets/user.svg";
 import sendIcon from "../assets/send-icon.svg";
+import ChatService from "../services/chat";
+import React, { useState, useEffect, useRef } from "react";
 
 function Chatbot() {
+  const [messages, setMessages] = useState([]);
+  const [userInput, setUserInput] = useState("");
+  const messagesEndRef = useRef(null);
+
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+    setUserInput("");
+    const userMessage = { user: true, message: userInput };
+    setMessages((prevMessages) => [...prevMessages, userMessage]);
+    const response = await ChatService.question(userInput);
+    const botMessage = { user: false, message: response.answer };
+    setMessages((prevMessages) => [...prevMessages, botMessage]);
+  };
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
-    <div className="w-full bg-chatbot">
+    <div className="w-full min-h-screen flex flex-col bg-chatbot">
+      {/* Header Section */}
       <div className="flex justify-center bg-header-next h-16">
-        <div id="header">
+        <div id="header" className="flex items-center">
           <img src={logoPacho} alt="logo" className="w-20" />
-          <div className="block">
-            <p className="text-2xl/6 text-black font-bold leading-9">
-              NOVA NextBot{" "}
-              <span className="text-white text-sm block font-normal">
-                En linea
-              </span>
+          <div className="ml-4">
+            <p className="text-2xl font-bold text-black">
+              NOVA NextBot
+              <span className="block text-sm text-white">En línea</span>
             </p>
           </div>
         </div>
       </div>
-      <div className="flex justify-center py-2" id="chat-container">
-        <div className="w-1/2" id="chat">
-          <div className="flex justify-end py-2">
-            <div className="text-white p-2 me-2 chat-container-user">
-              Hola, ¿En qué puedo ayudarte?
+
+      {/* Chat Container */}
+      <div 
+        className="px-4"
+        id="chat-container">
+        <div className="w-1/2 mx-auto" id="chat">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`flex justify-${message.user ? "end" : "start"} py-2`}
+            >
+              {message.user ? (
+                <>
+                  <div className="text-white p-2 me-2 rounded-lg chat-container-user">
+                    {message.message}
+                  </div>
+                  <img
+                    src={userIcon}
+                    className="rounded-full icon-user"
+                  />
+                </>
+              ) : (
+                <>
+                  <img
+                    src={logoPacho}
+                    className="rounded-full icon-bot me-2"
+                  />
+                  <div className="text-dark p-2 rounded-lg chat-container-bot">
+                    {message.message}
+                  </div>
+                </>
+              )}
             </div>
-            <img src={userIcon} className="rounded-circle icon-user" />
-          </div>
-          <div className="flex justify-start content-end py-2">
-            <img src={logoPacho} className="rounded-circle icon-bot me-2" />
-            <div className="text-dark p-2 chat-container-bot">
-              El grupo estudiantil NOVA es el 13avo grupo del 5to piso en el
-              bloque 29. Se centra en tecnología e innovación, buscando fomentar
-              un entorno para abordar problemas cotidianos de manera creativa.
-              NOVA está compuesto por 4 departamentos: RRPP, Mercadeo, Gestión
-              Humana y Comunidades. Fue creado el 19 de octubre de 2022 con el
-              propósito de ofrecer soluciones innovadoras.
-            </div>
-          </div>
+          ))}
+          <div ref={messagesEndRef} />
         </div>
       </div>
-      <div className="fixed bottom-4 w-full flex justify-center">
-        <div className="w-1/2" id="chat-input">
-          <form>
+
+      {/* Input Section */}
+      <div className="w-full flex justify-center p-2 fixed bottom-0">
+        <div className="w-1/2 mx-auto" id="chat-input">
+          <form onSubmit={handleSendMessage}>
             <div className="flex">
               <input
                 type="text"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                className="w-full p-2 border rounded-md focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="Escribe un mensaje..."
+                value={userInput}
+                onChange={(e) => setUserInput(e.target.value)}
               />
               <button
                 type="submit"
-                className="ml-2 mt-1 inline-flex items-center justify-center px-4 py-2 rounded-md border border-transparent shadow-sm text-sm font-medium text-header-next hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="ml-2 p-2 bg-white text-white rounded-md hover:bg-gray-100"
               >
                 <img src={sendIcon} alt="send" className="w-6" />
               </button>
