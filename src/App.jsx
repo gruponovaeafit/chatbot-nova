e59e6c0f61b06e7376e8bcc04fc2a552d75f0742
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-11 11:11:22 -0500

    Delete unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Nova from "./views/Nova";
 import Next from "./views/Next";
-import Omegahack from "./views/Omegahack";
-import { useViewportHeight } from "./utils/useViewportHeight";
 import "./App.css";
 
 export default function App() {
-  const height = useViewportHeight();
-
   return (
     <div className="font-inter supports-[height:100cqh]:h-[100cqh] supports-[height:100svh]:h-[100svh]">
       <Next />
     </div>
-  );
-}
-
-function AppContent() {
-  return (
-    <Routes>
-      <Route path="/" exact element={<Nova />} />
-      <Route path="/next" element={<Next />} />
-      <Route path="/omegahack" element={<Omegahack />} />
-    </Routes>
   );
 }
